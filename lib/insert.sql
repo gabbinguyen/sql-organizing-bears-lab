8bf99d9ea99d4f6862d5a0f0d3cfcd9f62d9e9d3
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-10-15 18:13:08 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('a', 1, 'f', 'bl', 'nice', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('b', 2, 'f', 'br', 'nice', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('c', 3, 'f', 'p', 'nice', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('d', 4, 'f', 'gr', 'nice', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('e', 5, 'm', 'bl', 'mean', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('f', 6, 'm', 'br', 'mean', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES ('g', 7, 'm', 'p', 'mean', 'y');
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) 
+--     VALUES (null, null, 'm', 'gr', 'mean', 'y');
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
